c58a967154821676c56cd62db974a0c5cf95b6e1
agungdh <agungdh@live.com>
2018-03-06 14:18:17 +0700

    init awal (blm full)

---
+++ app/src/main/java/com/agungdh/ipchecker/MainActivity.java
@@ -0,0 +1,20 @@
+package com.agungdh.ipchecker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView view = (WebView) this.findViewById(R.id.webView);
+        view.setWebViewClient(new WebViewClient());
+        view.loadUrl("https://agungdh.com");
+        view.getSettings().setJavaScriptEnabled(true);
+    }
+}
